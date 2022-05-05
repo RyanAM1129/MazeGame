29f9c08662cdbc3c919c6a044954427793898922
RyanAM1129 <ryanmac1@uw.edu>
2022-05-05 14:16:03 -0700

    first commit on new branch

---
+++ src/model/MultipleChoice.java
@@ -0,0 +1,60 @@
+package model;
+
+/**
+ * A class representing a Multiple Choice Question.
+ */
+public class MultipleChoice extends Question {
+    /**
+     * An array storing the three wrong answers.
+     * Should never be larger than 3.
+     */
+    private final String[] myWrong;
+
+    /**
+     * Constructor that receives three String Literals as parameters
+     * for the Wrong Answers.
+     *
+     * @param theQuestion the Question String.
+     * @param theAnswer the Answer String.
+     * @param theWrong1 the first Wrong Answer String.
+     * @param theWrong2 the second Wrong Answer String.
+     * @param theWrong3 the third Wrong Answer String.
+     */
+    public MultipleChoice(final String theQuestion, final String theAnswer,
+                          final String theWrong1, final String theWrong2,
+                          final String theWrong3) {
+        super(theQuestion, theAnswer);
+        myWrong = new String[] {theWrong1, theWrong2, theWrong3};
+    }
+
+    /**
+     * Constructor that receives an array of Strings as parameters
+     * for the Wrong Answers.
+     *
+     * @param theQuestion the Question String.
+     * @param theAnswer the Answer String.
+     * @param theWrong the array of Wrong Answer Strings.
+     */
+    public MultipleChoice(final String theQuestion, final String theAnswer,
+                          final String[] theWrong) {
+        super(theQuestion, theAnswer);
+        myWrong = theWrong.clone();
+    }
+
+    /**
+     * Returns all the wrong answers as an array.
+     * @return the array of Wrong Answers.
+     */
+    public String[] getWrongSet() {
+        return myWrong.clone();
+    }
+
+    /**
+     * Returns the Wrong Answer at a given index.
+     * @param index the index of the Wrong Answer.
+     * @return the Wrong Answer at index 'index'.
+     */
+    public String getWrong(final int index) {
+        return myWrong[index];
+    }
+}
---
+++ src/model/Question.java
@@ -0,0 +1,44 @@
+package model;
+
+/**
+ * A class representing a Question for a trivia game.
+ */
+public class Question {
+    /**
+     * A string containing the 'Question'.
+     */
+    private final String myQuestion;
+    /**
+     * A string containing the 'Answer'.
+     */
+    private final String myAnswer;
+
+    /**
+     * Constructor for Question using a given Question and Answer.
+     *
+     * @param theQuestion the given question.
+     * @param theAnswer the given answer.
+     */
+    public Question(final String theQuestion, final String theAnswer) {
+        myQuestion = theQuestion;
+        myAnswer = theAnswer;
+    }
+
+    /**
+     * Returns the Question String.
+     *
+     * @return the Question String.
+     */
+    public String getQuestion() {
+        return myQuestion;
+    }
+
+    /**
+     * Returns the Answer String.
+     *
+     * @return the Answer String.
+     */
+    public String getAnswer() {
+        return myAnswer;
+    }
+}
