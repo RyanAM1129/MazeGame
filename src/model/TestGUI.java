3d61f3cb53de9b95cdb78459c34d68d7cea9bb36
Ye Li <llyy1404@gmail.com>
2022-05-07 00:12:29 -0700

    decide switch menu bar method to using button in main page, create method that display panel once click on start

--- src/model/TestGUI.java
+++ src/model/TestGUI.java
@@ -1,66 +1,100 @@
 import javax.swing.*;
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class TestGUI extends JFrame implements ActionListener {
+    JFrame window;
+    Container con;
+    JPanel titleNamePanel,startButtonPanel, QuestionPanel,choiceButtonPanel;
+    JLabel titleNameLabel;
 
-    JMenuBar menuBar;
-    JMenu file, about;
-    JMenuItem start,exit,info,team;
-
-
-    public TestGUI() {
-        menuBar = new JMenuBar();
-
-
-        file = new JMenu("File");
-        menuBar.add(file);
-        start = new JMenuItem("Start");
-        exit = new JMenuItem("Exit");
-        file.add(start);
-        file.add(exit);
-
-        about = new JMenu("About");
-        menuBar.add(about);
-        info = new JMenuItem("info");
-        team = new JMenuItem("team");
-        about.add(info);
-        about.add(team);
-        exit.addActionListener(this);
-        JLabel Title = new JLabel("Trivia Maze");
-        Title.setHorizontalAlignment(SwingConstants.CENTER);
-        Title.setFont(new Font("Arial", Font.BOLD, 40));
-
-        this.add(Title);
-        this.setJMenuBar(menuBar);
+    JButton startButton,aboutButton,teamButton,exitButton,choice1,choice2,choice3,choice4;
+    Font normalFont = new Font("Times New Roman",Font.PLAIN,50);
 
 
+    public TestGUI(){
+        window = new JFrame();
+        window.setSize(800,600);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLayout(null);
+
+        con = window.getContentPane();
+
+        titleNamePanel = new JPanel();
+        titleNamePanel.setBounds(100,100,600,150);
+        titleNameLabel = new JLabel("Trival Maze");
+
+        titleNameLabel.setForeground(Color.BLUE);
+        titleNameLabel.setFont(new Font("Times New Roman",Font.PLAIN,90));
+
+
+        startButtonPanel = new JPanel();
+        startButtonPanel.setBounds(300,400,200,100);
+
+        startButton = new JButton ("START");
+        aboutButton = new JButton ("ABOUT");
+        teamButton = new JButton ("TEAM");
+        exitButton = new JButton ("EXIT");
+        startButton.addActionListener(this);
+        exitButton.addActionListener(this);
+
+        titleNamePanel.add(titleNameLabel);
+        startButtonPanel.add(startButton);
+        startButtonPanel.add(teamButton);
+        startButtonPanel.add(aboutButton);
+        startButtonPanel.add(exitButton);
+        con.add(titleNamePanel);
+        con.add(startButtonPanel);
+
+        window.setVisible(true);
     }
+    public void createGameScreen(){
+        titleNamePanel.setVisible(false);
+        startButtonPanel.setVisible(false);
+        QuestionPanel = new JPanel();
+        QuestionPanel.setBounds(100,100,600,250);
+        QuestionPanel.setBackground(Color.black);
+        con.add(QuestionPanel);
 
-    public static void main (String[] args){
-        TestGUI frame = new TestGUI();
-        frame.setVisible(true);
-        frame.setSize(800,600);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-
+        choiceButtonPanel = new JPanel();
+        choiceButtonPanel.setBounds(250,350,300,150);
+        choiceButtonPanel.setLayout(new GridLayout(4,1));
+        con.add(choiceButtonPanel);
+        choice1 = new JButton("A");
+        choice1.setBackground(Color.BLACK);
+        choice1.setFont(normalFont);
+        choiceButtonPanel.add(choice1);
+        choice2 = new JButton("B");
+        choice2.setBackground(Color.BLACK);
+        choice2.setFont(normalFont);
+        choiceButtonPanel.add(choice2);
+        choice3 = new JButton("C");
+        choice3.setBackground(Color.BLACK);
+        choice3.setFont(normalFont);
+        choiceButtonPanel.add(choice3);
+        choice4 = new JButton("D");
+        choice4.setBackground(Color.BLACK);
+        choice4.setFont(normalFont);
+        choiceButtonPanel.add(choice4);
+        con.add(choiceButtonPanel);
     }
-
-    @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == start){
-            //pop up maze panel or question panel
+        if(e.getSource() == startButton){
+            createGameScreen();
         }
-        if(e.getSource() == exit){
+        if(e.getSource() == exitButton){
             System.exit(0);
         }
-        if(e.getSource() == info){
+        if(e.getSource() == teamButton){
             //pop up how maze will work
         }
-        if(e.getSource() == team){
+        if(e.getSource() == aboutButton){
             //team info
         }
+    };
+    public static void main (String[] args){
+        new TestGUI();
     }
 }
+
